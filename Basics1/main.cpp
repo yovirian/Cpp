08e6c0bd49d61a5c84629873187bdddc063bd263
yovirian <113366689+yovirian@users.noreply.github.com>
2024-07-12 19:39:44 +0530

    Add files via upload

---
+++ Basics1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+//types of datatypes
+    cout <<sizeof(x)<<endl;
+    cout<<sizeof(float)<<endl;
+    cout<<sizeof(char)<<endl;
+    cout<<sizeof(double)<<endl;
+    cout<<sizeof(bool)<<endl;
+
+    return 0;
+}
