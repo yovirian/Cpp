87c545ebda53730559369ef3814403f66b29f99f
yovirian <113366689+yovirian@users.noreply.github.com>
2024-07-18 00:25:15 +0530

    Add files via upload

---
+++ Basics2/Basics1.cpp
@@ -0,0 +1,33 @@
+//prepocessor | Headerfile
+# include <iostream>
+//C++ standard namespace
+using namespace std;
+
+//Return type | Entry point
+int main()
+{
+    //function is under curly braces
+
+
+    //variable input
+    int marks;
+    //input
+    cin >> marks;
+    cout <<"Marks "<<marks<<endl;
+
+    int b,c;
+    //variable declarartion
+    int a = 10;
+    cin >> b>> c;
+    //addition operator
+    cout<<"Sum is " << a+b+c <<endl;
+/* operator:-division /
+ * modulus%
+ * multiplication *
+ * increment ++ */
+
+//successful execution
+    return 0;
+}
+
+
