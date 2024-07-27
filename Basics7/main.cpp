1f90c5545d16e71887b1a73d4c8cc9dade6df7d2
yovirian <113366689+yovirian@users.noreply.github.com>
2024-07-27 10:18:09 +0530

    Add files via upload

---
+++ Basics7/main.cpp
@@ -0,0 +1,13 @@
+
+    int NO = 10;// statements
+
+    5+3//expression
+    5+3;// statement
+    int a = 5+3;//statement
+    int total = NO;// statement
+
+    //conditional statements
+    if(....){
+
+    }
+
